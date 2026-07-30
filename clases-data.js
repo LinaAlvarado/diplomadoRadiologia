b2c42576d716ee61ba928837fe9cb3681cd08ef0
LinaAlvarado <linamariaalvaradopallares@gmail.com>
2026-07-30 17:09:19 -0500

    update

--- clases-data.js
+++ clases-data.js
@@ -80,7 +80,7 @@ const clases = [
     tema: "Anatomía radiológica de abdomen",
     docente: "Sebastián García Patiño",
     fecha: "2026-07-30T17:00:00-05:00",
-    link: ""
+    link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWFiZjU1YjEtY2UyYi00M2ZmLWFmYjgtYzcxYzI1MzM1YTE5%40thread.v2/0?context=%7b%22Tid%22%3a%224f637f79-0a56-410b-9b1e-0a776875c2af%22%2c%22Oid%22%3a%22ad9bcd70-3a3a-4483-811b-c7176ef6fbb5%22%7d"
     // demoHoy: true
   },
   {
